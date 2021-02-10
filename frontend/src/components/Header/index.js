627b583cc91070d25d16f1950e70d8783aa5d9ed
julienmtau7 <mtaujulien@gmail.com>
2021-02-10 14:50:38 +0100

    setting up the drop-down menu on mobile and a beginning of style

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 // == Import Scss
 import './header.scss';
 
-const Header = () => (
-  <header className="header">
-    <div className="header__title">
-      <h1>Crypto Match</h1>
-      <button className="buttonDesktop" type="button">Connexion</button>
-      <button className="buttonDesktop" type="button">Inscription</button>
-      <FontAwesomeIcon size="2x" icon={faBars} />
-    </div>
-    <nav>
-      <ul>
-        <li>Accueil</li>
-        <li>Cryptomonnaies</li>
-        <li>Classement</li>
-        <li>Qui-sommes-nous</li>
-      </ul>
-      <button className="buttonMobile" type="button">Connexion</button>
-      <button className="buttonMobile" type="button">Inscription</button>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const classNav = isOpen ? 'header__nav-open' : 'header__nav-close';
+  return (
+
+    <header className="header">
+      <div className="header__title">
+        <h1>Crypto Match</h1>
+        <button className="buttonDesktop" type="button">Connexion</button>
+        <button className="buttonDesktop" type="button">Inscription</button>
+        <button
+          className="buttonMenuBurger"
+          type="button"
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <FontAwesomeIcon
+            className="iconBurger"
+            size="2x"
+            icon={faBars}
+          />
+        </button>
+      </div>
+      <nav className={classNav}>
+        <ul>
+          <li>Accueil</li>
+          <li>Cryptomonnaies</li>
+          <li>Classement</li>
+          <li>Qui-sommes-nous</li>
+        </ul>
+        <div className="buttonLogin">
+          <button className="buttonMobile login" type="button">Connexion</button>
+          <button className="buttonMobile signin" type="button">Inscription</button>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+// == Import Scss
+import './home.scss';
+
 const Home = () => (
-  <div className="Home">
-    <h1>Home</h1>
+  <div className="home">
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis commodi earum, ipsam saepe cum est facilis. Consequatur, incidunt natus animi illo, qui quasi, id amet omnis enim ratione atque beatae.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis commodi earum, ipsam saepe cum est facilis. Consequatur, incidunt natus animi illo, qui quasi, id amet omnis enim ratione atque beatae.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis commodi earum, ipsam saepe cum est facilis. Consequatur, incidunt natus animi illo, qui quasi, id amet omnis enim ratione atque beatae.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis commodi earum, ipsam saepe cum est facilis. Consequatur, incidunt natus animi illo, qui quasi, id amet omnis enim ratione atque beatae.
+    </p>
   </div>
 );
 
