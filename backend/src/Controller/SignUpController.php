69048ace4be8bc5f32331b0478ba863d4c7ec0d6
Rootbu <mr.moussawalid@gmail.com>
2021-02-11 12:37:05 +0100

    test signup and send token..

--- backend/src/Controller/SignUpController.php
+++ backend/src/Controller/SignUpController.php
@@ -7,15 +7,17 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 
 class SignUpController extends AbstractController
 {
     /**
      * @Route("/signup", name="apiSignUp")
      */
-    public function signup(Request $Request, UserPasswordEncoderInterface $Encoder): Response
+    public function signup(Request $Request, UserPasswordEncoderInterface $Encoder, JWTTokenManagerInterface $JWTManager): Response
     {
 
         $Date = new DateTime();
@@ -40,11 +42,8 @@ class SignUpController extends AbstractController
         $Em->persist($User);
         $Em->flush();
 
+        return new JsonResponse(json_encode( ['token' => $JWTManager->create($User)]) );
 
-        return $this->json([
-            'message' => 'Welcome to your new contr!',
-            'path' => 'src/Controller/SingUpController.php',
-        ]);
     }
 
     public function getTokenUser(UserInterface $user, JWTTokenManagerInterface $JWTManager)
