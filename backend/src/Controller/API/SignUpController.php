dab8cabdd63dee315ece0a67e6f3d53c8a5dbff1
Codeur Chris <codeur.chris@gmail.com>
2021-02-11 11:12:51 +0100

    rename signupController file

--- backend/src/Controller/API/SignUpController.php
+++ backend/src/Controller/API/SignUpController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SingUpController extends AbstractController
+class SignUpController extends AbstractController
 {
     /**
      * @Route("/api/v1/signup", name="apiSignUp")
