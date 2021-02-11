422187b4b837ac3255c1fb03f1f0aedf77a16b56
JocelynLebaad <lebaad.jocelyn@gmail.com>
2021-02-11 14:44:19 +0100

    page connexion

---
+++ frontend/src/components/Connexion/SignIn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const SignIn = () => (
+  <div className="signIn">
+    <input name="nom" />
+    <label>Nom</label>
+  </div>
+);
+
+export default SignIn;
---
+++ frontend/src/components/Connexion/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import SignIn from './SignIn';
+// import SignUp from './SignUp';
+
+const Connexion = () => (
+  <div className="connexion">
+    <SignIn />
+  </div>
+);
+
+export default Connexion;
