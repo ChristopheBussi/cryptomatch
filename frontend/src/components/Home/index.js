4a29f5c461b4aaf163caf401740450cc89e32338
JocelynLebaad <lebaad.jocelyn@gmail.com>
2021-02-11 13:59:38 +0100

    design page home fix

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -5,7 +5,18 @@ import './home.scss';
 
 const Home = () => (
   <div className="home">
-    
+    <h2>Bienvenue sur Crypto Match</h2>
+    <p>
+      Le site organise un concours de trading sur les cryptomonnaies sans argent réel.
+    </p>
+    <p>
+      Chaque participant obtiendra un capital de départ de 10 000 dollars fictifs qu’il pourra
+      investir sur les cryptomonnaies de son choix.
+    </p>
+    <p>
+      Le gagnant est celui qui aura engendré la plus grande plus-value au bout d'un mois.
+    </p>
+
   </div>
 );
 
