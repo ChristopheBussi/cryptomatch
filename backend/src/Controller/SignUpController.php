79ab2a56976bac00bdc0729d0b581c74d5951476
Codeur Chris <codeur.chris@gmail.com>
2021-02-13 15:16:58 +0100

    signup responses messages in french

--- backend/src/Controller/SignUpController.php
+++ backend/src/Controller/SignUpController.php
@@ -7,11 +7,9 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
@@ -20,93 +18,84 @@ class SignUpController extends AbstractController
     /**
      * @Route("/signup", name="apiSignUp")
      */
-    public function signup(Request $Request, UserPasswordEncoderInterface $Encoder, JWTTokenManagerInterface $JWTManager,
-    ValidatorInterface $Validator): Response
+    public function signup(Request $Request, UserPasswordEncoderInterface $Encoder, ValidatorInterface $Validator): Response
     {
         $Response = new Response();
 
         if($Request->getContent() != null)
         {
-                    //Get request data and convert in array...
-                    $Data = (array) json_decode($Request->getContent());
-            
-                if(isset($Data['email']) && isset($Data['password']) && isset($Data['username']) )
+            //Get request data and convert in array
+            $Data = (array) json_decode($Request->getContent());
+        
+            if(isset($Data['email']) && isset($Data['password']) && isset($Data['username']) )
+            {
+                $Date = new DateTime();
+                $User = new User();
+
+                //Verification valid email
+                $EmailConstraint = new Assert\Email();
+                $EmailConstraint->message = 'Adresse e-mail invalide.';
+                $Errors = $Validator->validate($Data['email'], $EmailConstraint);
+
+                if(count($Errors) == 0)
                 {
-                    $Date = new DateTime();
-                    $User = new User();
+                    //Setter user...
+                    $User->setEmail($Data['email']);
 
+                    //Verify if username exists
+                    $Em = $this->getDoctrine()->getManager();
+                    $RepUser = $Em->getRepository(User::class);
+                    $UserExist = $RepUser->findOneBy(['username' => $Data['username'] ] );
 
+                    if(!$UserExist) {
+                    
+                        $User->setUsername($Data['username']);
+                        $Pass = $Encoder->encodePassword($User, $Data['password']);
+                        $User->setPassword($Pass);
+                        $User->setCreatedAt($Date);
+                        $User->setUSDAmount(0);
+                        $User->setRankValorisationAmount(0);
 
-                    //Vérification valid email...
-                    $EmailConstraint = new Assert\Email();
-                    $EmailConstraint->message = 'Adresse mail invalide.';
-                    $Errors = $Validator->validate($Data['email'], $EmailConstraint);
+                        $Em->persist($User);
+                        $Em->flush();
 
-                    if(count($Errors) == 0)
-                    {
-                        //Setter user...
-                        $User->setEmail($Data['email']);
-
-                        //Verification Username if exist.. 
-                        $Em = $this->getDoctrine()->getManager();
-
-                        $RepUser = $Em->getRepository(User::class);
-                        $UserExist = $RepUser->findOneBy(['username' => $Data['username'] ] );
-                        if(!$UserExist)
-                        {
-                        
-                            $User->setUsername($Data['username']);
-                            $Pass = $Encoder->encodePassword($User, $Data['password']);
-                            $User->setPassword($Pass);
-                            $User->setCreatedAt($Date);
-                            $User->setUSDAmount(0);
-                            $User->setRankValorisationAmount(0);
-
-
-                            $Em->persist($User);
-                            $Em->flush();
-
-                            $Response->setStatusCode("201");
-                            $Response->setCOntent(json_encode(array(
-                                'Message' => 'Inscription réussie'
-                            )));
-                        }
-                        else
-                        {
-                            $Response->setStatusCode("403");
-                            $Response->setCOntent(json_encode(array(
-                                'Message' => 'User already exist'
-                            )));
-                        }
+                        $Response->setStatusCode("201");
+                        $Response->setContent(json_encode(array(
+                            'Message' => 'Inscription réussie'
+                        )));
                     }
                     else
                     {
-                        $Response->setStatusCode("400");
-                        $Response->setCOntent(json_encode(array(
-                            'Message' => $Errors[0]->getMessage()
+                        $Response->setStatusCode("403");
+                        $Response->setContent(json_encode(array(
+                            'Message' => 'Ce nom d\'utilisateur existe déjà.'
                         )));
                     }
-
                 }
                 else
                 {
-                    $Response->setStatusCode("500");
-                    $Response->setCOntent(json_encode(array(
-                        'Message' => 'Invalid Request'
+                    $Response->setStatusCode("400");
+                    $Response->setContent(json_encode(array(
+                        'Message' => $Errors[0]->getMessage()
                     )));
                 }
-    
+            }
+            else
+            {
+                $Response->setStatusCode("500");
+                $Response->setContent(json_encode(array(
+                    'Message' => 'Requête invalide'
+                )));
+            }
         }
         else
         {
             $Response->setStatusCode("500");
-            $Response->setCOntent(json_encode(array(
-                'Message' => 'Request null'
+            $Response->setContent(json_encode(array(
+                'Message' => 'Requête nulle'
             )));
         }
         
         return $Response;
     }
-
-  
 }
