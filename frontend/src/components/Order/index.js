4f0542fe1d7f823ad58334af1e6798599b58ad4b
JocelynLebaad <lebaad.jocelyn@gmail.com>
2021-02-19 19:48:12 +0100

    styled order

--- frontend/src/components/Order/index.js
+++ frontend/src/components/Order/index.js
@@ -41,30 +41,36 @@ class Order extends Component {
       event.preventDefault();
       handlePlaceTheOrder(this.state.typeAction, this.state.quotation);
     };
+    const Amount = Math.round(USDAmount * 100) / 100;
     const displaymMessage = message != null ? 'order__messageDisplay' : 'order__messageNone';
     return (
       <div className="order">
         <h2 className="order__title">{pairname}</h2>
         <span className="order__quotation"></span>
-        <Graphic pairName={pairname} />
-        <div className={displaymMessage}>{message}</div>
-        <div className="order__usdAmout">Montant USDT disponible : {USDAmount} </div>
-        <div className="order__cryptoAmount">Quantité de {pairname}  detenus : {actualQuantityPair} </div>
-        <form onSubmit={handleSubmit}>
-          <Field
-            name="quantity"
-            type="number"
-            placeholder="Quantité"
-            value={quantity}
-            onChange={changeField}
-          />
-          <button type="submit" onClick={() => this.state.typeAction = 'Buy'}>
-            Acheter
-        </button>
-          <button type="submit" onClick={() => this.state.typeAction = 'Sell'}>
-            Vendre
-        </button>
-        </form>
+        <div className="order__graph">
+          <Graphic pairName={pairname} />
+          <div className="order__passed">
+            <div className={displaymMessage}>{message}</div>
+            <div className="order__usdAmout">Montant USDT disponible : {Amount.toLocaleString()} </div>
+            <div className="order__cryptoAmount">Quantité de {pairname}  detenus : {actualQuantityPair} </div>
+            <form onSubmit={handleSubmit}>
+              <Field
+                name="quantity"
+                type="number"
+                placeholder="Quantité"
+                value={quantity}
+                onChange={changeField}
+              />
+              <button type="submit" onClick={() => this.state.typeAction = 'Buy'}>
+                Acheter
+             </button>
+              <button type="submit" onClick={() => this.state.typeAction = 'Sell'}>
+                Vendre
+              </button>
+            </form>
+          </div>
+
+        </div>
       </div>
     );
   }
